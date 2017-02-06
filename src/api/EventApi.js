4cf90d3c46b6e67312f80e1deb0e4a9f2ad16b18
Johan Lejdung <johanlejdung@gmail.com>
2017-02-06 15:08:26 +0100

    Starting change

--- src/api/EventApi.js
+++ src/api/EventApi.js
@@ -3,7 +3,11 @@ import Event from '../models/Event';
 export default class EventApi {
   getEvent(uuid) {
     return new Promise((resolve, reject) => {
-        resolve(new Event);
+    	var event = new Event(eventid, "Awesome Event", null, "This is a description of the awesome event. It will take place in... YOUR PANTS BADUMM-DSHHH!");
+
+    	etTimeout(function() {
+        	resolve(event);
+        }, 1000);
     });
   }
 }
