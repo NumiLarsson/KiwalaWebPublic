974e94928238163a92713640ebcccf9fdd16c0c0
Johan Lejdung <johanlejdung@gmail.com>
2017-02-07 09:21:06 +0100

    added load random event button

--- src/containers/example.js
+++ src/containers/example.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {increaseCounter, updateTitle, updateTitleAsync} from '../actions/example';
+import {increaseCounter, updateTitle, updateTitleAsync, loadRandomEvent} from '../actions/example';
 import './styles/example.css';
 
 class Example extends Component {
@@ -10,6 +10,7 @@ class Example extends Component {
         this.increaseCounter = this.increaseCounter.bind(this);
         this.changeTitle = this.changeTitle.bind(this);
         this.changeTitleAsync = this.changeTitleAsync.bind(this);
+        this.loadRandomEvent = this.loadRandomEvent.bind(this);
     }
 
     increaseCounter() {
@@ -25,12 +26,18 @@ class Example extends Component {
         this.props.updateTitleAsync(this.props.title);
     }
 
+    loadRandomEvent() {
+        this.props.loadRandomEvent();
+    }
+
     render() {
         return (
             <div className="example">
                 <h1>This is the example container</h1>
                 <h2>Title: {this.props.title} </h2>
                 <h2>Counter: {this.props.counter} </h2>
+                <button onClick={this.loadRandomEvent}>Load random event</button>
+                <br/>
                 <button onClick={this.increaseCounter}>Increase counter</button>
                 <label htmlFor="title">Type in the field to change the title.</label> 
                 <input name="title" type="text" value={this.props.title} onChange={this.changeTitle} />
@@ -53,7 +60,8 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = {
     increaseCounter,
     updateTitle,
-    updateTitleAsync
+    updateTitleAsync,
+    loadRandomEvent
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Example);
