8d0fdb58cf7ffdb4642b325252a869d19b677931
Johan Lejdung <johanlejdung@gmail.com>
2017-02-23 16:52:14 +0100

    added reducer

---
+++ src/reducers/eventmodules.js
@@ -0,0 +1,24 @@
+import { EVENT_ACTIONS } from '../actions/actionTypes';
+import { MAPS_ACTIONS } from '../actions/maps';
+const { SET_CURRENT_EVENT_MODULES } = EVENT_ACTIONS;
+
+const initialState = {
+    loaded: null, 
+    description: null,
+    details: null,
+    participants: null
+}
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+
+        case SET_CURRENT_EVENT_MODULES:
+
+            return Object.assign({}, state, {
+                ...action.payload,
+                loaded: true
+            });
+        default:
+            return state;
+    }
+}
