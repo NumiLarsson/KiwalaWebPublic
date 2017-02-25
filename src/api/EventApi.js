5faa437e0152b25079fa4d396e2250b5c7af08de
Jonas Olander <jonas.olander91@gmail.com>
2017-02-25 08:16:45 +0100

    Added functions for updating event data and the eventModule settings

--- src/api/EventApi.js
+++ src/api/EventApi.js
@@ -1,235 +1,278 @@
-export default class EventApi {
+export default class EventApi {  
+  
+	constructor(database) {
+		this.database = database;
+		this.subscriptions = {};
+	}
+
+	/**
+	 * Get an event.
+	 * @param {string} eventId - The id of the event.
+	 * @returns A Promise which resolves to an event object.
+ 	 */
+	getEvent(eventId) {
+		let self = this;
+		return new Promise((resolve, reject) =>{
+			self.database().ref(`/events/${eventId}`).once('value')
+			.then(snapshot => {
+				let event = snapshot.val();
+
+				if(event) {
+					resolve(event);
+				} else {
+					reject('No event with that id exists');
+				}
+			})
+			.catch(err => {
+				reject(err);
+			})
+		})
+	}
+
+	/**
+	 * Subsribe to changes to an event.
+	 * @param {string} eventId - The id of the event.
+	 * @param {function} callback - Callback function which is called with the updated event object.
+ 	 */
+	subscribeToEvent(eventId, callback) {
+	  let ref = this.database().ref(`/events/${eventId}`)
+		ref.on('value', snapshot => {
+			callback(snapshot.val());
+		});
+		this.subscriptions[`event_${eventId}`] = ref;
+	}
+	
+	/**
+	 * Get the data for an event
+	 * @param {string} eventId - The id of the event.
+	 * @returns A Promise which resolves to an eventData object
+ 	 */
+	getEventData(eventId) {
+		let self = this;
+		return new Promise((resolve, reject) =>{
+			self.database().ref(`/eventData/${eventId}`).once('value')
+			.then(snapshot => {
+				let eventData = snapshot.val();
+
+				if(eventData) {
+					resolve(eventData);
+				} else {
+					reject('No data exists for that event');
+				}
+			})
+			.catch(err => {
+				reject(err);
+			})
+		})
+	}
+
+	/**
+	 * Subsribe to changes to eventData.
+	 * @param {string} eventId - The id of the event.
+	 * @param {function} callback - Callback function which is called with the updated eventData object.
+ 	 */
+	subscribeToEventData(eventId, callback) {
+		let ref = this.database().ref(`/eventData/${eventId}`);
+		ref.on('value', snapshot => {
+				callback(snapshot.val());
+		});
+		this.subscriptions[`eventData_${eventId}`] = ref;
+	}
+
+	/**
+	 * Update event data.
+	 * @param {string} eventId - The id of the event.
+	 * @param {object} dataUpdates - Object mapping the name of the data field to the new value.
+	 * @example updateEventData(2. {description: 'New event description'})
+ 	 */
+	updateEventData(eventId, dataUpdates) {
+		let self = this;
+		return new Promise((resolve, reject) => {
+			let updates = {}
+			for key in Object().key(dataUpdates) {
+				updates[`eventData/${eventId}/${key}`] = dataUpdates[key];
+			}
+			self.database().ref.update(updates)
+			.then(() => {
+				resolve('SUCCESS');
+			})
+			.catch(err => {
+				reject(err);
+			});
+		});
+	}
+
+	/**
+	 * Get the module settings for an event.
+	 * @param {string} eventId - The id of the event.
+	 * @return Promise which resolve to an eventModules object and reject with an error.
+ 	 */
+	getEventModules(eventId) {
+		let self = this;
+		return new Promise((resolve, reject) =>{
+			self.database().ref(`/eventModules/${eventId}`).once('value')
+			.then(snapshot => {
+				let eventModules = snapshot.val();
+
+				if(eventModules) {
+					resolve(eventModules);
+				} else {
+					reject('No modules exists for that event');
+				}
+			})
+			.catch(err => {
+				reject(err);
+			})
+		})
+	}
+
+	/**
+	 * Subsribe to changes to the modules of an event.
+	 * @param {string} eventId - The id of the event.
+	 * @param {function} callback - Callback function which is called with the updated eventModules object.
+ 	 */
+	subscribeToEventModules(eventId, callback) {
+		let ref = this.database().ref(`/eventModules/${eventId}`);
+		ref.on('value', snapshot => {
+				callback(snapshot.val());
+		});
+		this.subscriptions[`eventModules_${eventId}`] = ref;
+	}
 
 
-  constructor(database) {
-    this.database = database;
-    this.subscriptions = {};
-  }
+	/**
+	 * Update the settings for an event module.
+	 * @param {string} eventId - The id of the event.
+	 * @param {string} module - The name/id of the module.
+	 * @param {object} settings - The values to be saved.
+ 	 */
+	UpdateEventModuleSettings(eventId, module, settings) {
+		let self = this;
+		return new Promise((resolve, reject) => {
+			self.database().ref(`eventModules\${eventId}\${module}`).set(settings)
+			.then(() => {
+				resolve('SUCCESS');
+			})
+			.catch(err => {
+				reject(err);
+			})
+		})
+	}
 
-  /**
-   * Get an event.
-   * @param {string} eventId - The id of the event.
-   * @returns A Promise which resolves to an event object.
-   */
-  getEvent(eventId) {
-    let self = this;
-    return new Promise((resolve, reject) => {
-      self.database().ref(`/events/${eventId}`).once('value')
-        .then(snapshot => {
-          let event = snapshot.val();
+	/**
+	 * Get the participants for an event.
+	 * @param {string} eventId - The id of the event.
+	 * @return Promise which resolve to an eventParticipants object and reject with an error.
+ 	 */
+	getEventParticipants(eventId) {
+		let self = this;
+		return new Promise((resolve, reject) =>{
+			self.database().ref(`/eventParticipants/${eventId}`).once('value')
+			.then(snapshot => {
+				let eventParticipants = snapshot.val();
 
-          if (event) {
-            resolve(event);
-          } else {
-            reject('No event with that id exists');
-          }
-        })
-        .catch(err => {
-          reject(err);
-        })
-    })
-  }
+				if(eventParticipants) {
+					resolve(eventParticipants);
+				} else {
+					reject('No modules exists for that event');
+				}
+			})
+			.catch(err => {
+				reject(err);
+			})
+		})
+	}
 
-  /**
-   * Subsribe to changes to an event.
-   * @param {string} eventId - The id of the event.
-   * @param {function} callback - Callback function which is called with the updated event object.
-   */
-  subscribeToEvent(eventId, callback) {
-    let ref = this.database().ref(`/events/${eventId}`)
-    ref.on('value', snapshot => {
-      callback(snapshot.val());
-    });
-    this.subscriptions[`event_${eventId}`] = ref;
-  }
+	/**
+	 * Subsribe to changes to the participants of an event. USE WITH CARE
+	 * @param {string} eventId - The id of the event.
+	 * @param {function} callback - Callback function which is called with the updated eventParticipants object.
+ 	 */
+	subscribeToEventParticipants(eventId, callback) {
+		let ref = this.database().ref(`/eventParticipants/${eventId}`);
+		ref.on('value', snapshot => {
+			callback(snapshot.val());
+		});
+		this.subscriptions[`eventParticipants_${eventId}`] = ref;
+	}
 
-  /**
-   * Get the data for an event
-   * @param {string} eventId - The id of the event.
-   * @returns A Promise which resolves to an eventData object
-   */
-  getEventData(eventId) {
-    let self = this;
-    return new Promise((resolve, reject) => {
-      self.database().ref(`/eventData/${eventId}`).once('value')
-        .then(snapshot => {
-          let eventData = snapshot.val();
+	/**
+	 * Attend an event
+	 * @param {string} eventId - The id of the event.
+	 * @param {string} uid - The id of the user.
+	 * @returns A Promise which resolves to a success message and rejects with an error.
+ 	 */
+	attendEvent(eventId, uid) {
+		let self = this;
+		return new Promise((resolve, reject) => {
+			let updates = {};
+			updates[`/eventParticipants/${eventId}/${uid}`] = true;
+			updates[`/userAcceptedEvents/${uid}/${eventId}`] = true;
+			self.database().ref().update(updates)
+			.then(() => {
+				resolve('SUCCESS');
+			})
+			.catch(err => {
+				reject(err);
+			});
+		})
+	}
 
-          if (eventData) {
-            resolve(eventData);
-          } else {
-            reject('No data exists for that event');
-          }
-        })
-        .catch(err => {
-          reject(err);
-        })
-    })
-  }
+	/**
+	 * Unattend event
+	 * @param {string} eventId - The id of the event.
+	 * @param {string} uid - The id of the user.
+	 * @returns A Promise which resolves to a success message and rejects with an error.
+ 	 */
+	unattendEvent(eventId, uid) {
+		let self = this;
+		return new Promise((resolve, reject) => {
+			let updates = {};
+			updates[`/eventParticipants/${eventId}/${uid}`] = null;
+			updates[`/userAcceptedEvents/${uid}/${eventId}`] = null;
+	        self.database().ref().update(updates)
+	        .then(() => {
+	            resolve('SUCCESS');
+	        })
+	        .catch(err => {
+	            reject(err);
+	        });
+	    })
+	}
 
-  /**
-   * Subsribe to changes to eventData.
-   * @param {string} eventId - The id of the event.
-   * @param {function} callback - Callback function which is called with the updated eventData object.
-   */
-  subscribeToEventData(eventId, callback) {
-    let ref = this.database().ref(`/eventData/${eventId}`);
-    ref.on('value', snapshot => {
-      callback(snapshot.val());
-    });
-    this.subscriptions[`eventData_${eventId}`] = ref;
-  }
+	/**
+	 * Subsribe to all events.
+	 * @param {function} added - Function to be called when an event is added.
+	 * @param {function} changed - Function to be called when an event is changed.
+	 * @param {function} deleted - Function to be called when an event is deleted.
+ 	 */
+	subscribeToEventList(added, changed, deleted) {
+		let ref = this.database().ref('/events');
+		ref.on('child_added', (snapshot => {
+			added(snapshot.val());
+		}));
 
-  /**
-   * Get the module settings for an event.
-   * @param {string} eventId - The id of the event.
-   * @return Promise which resolve to an eventModules object and reject with an error.
-   */
-  getEventModules(eventId) {
-    let self = this;
-    return new Promise((resolve, reject) => {
-      self.database().ref(`/eventModules/${eventId}`).once('value')
-        .then(snapshot => {
-          let eventModules = snapshot.val();
+		ref.on('child_changed', (snapshot => {
+			changed(snapshot.val());
+		}));
 
-          if (eventModules) {
-            resolve(eventModules);
-          } else {
-            reject('No modules exists for that event');
-          }
-        })
-        .catch(err => {
-          reject(err);
-        })
-    })
-  }
+		ref.on('child_removed', (snapshot => {
+			deleted(snapshot.val());
+		}));
 
-  /**
-   * Subsribe to changes to the modules of an event.
-   * @param {string} eventId - The id of the event.
-   * @param {function} callback - Callback function which is called with the updated eventModules object.
-   */
-  subscribeToEventModules(eventId, callback) {
-    let ref = this.database().ref(`/eventModules/${eventId}`);
-    ref.on('value', snapshot => {
-      callback(snapshot.val());
-    });
-    this.subscriptions[`eventModules_${eventId}`] = ref;
-  }
+		this.subscriptions['eventList'] = ref;
+	}
 
-  /**
-   * Get the participants for an event.
-   * @param {string} eventId - The id of the event.
-   * @return Promise which resolve to an eventParticipants object and reject with an error.
-   */
-  getEventParticipants(eventId) {
-    let self = this;
-    return new Promise((resolve, reject) => {
-      self.database().ref(`/eventParticipants/${eventId}`).once('value')
-        .then(snapshot => {
-          let eventParticipants = snapshot.val();
-
-          if (eventParticipants) {
-            resolve(eventParticipants);
-          } else {
-            reject('No modules exists for that event');
-          }
-        })
-        .catch(err => {
-          reject(err);
-        })
-    })
-  }
-
-  /**
-   * Subsribe to changes to the participants of an event. USE WITH CARE
-   * @param {string} eventId - The id of the event.
-   * @param {function} callback - Callback function which is called with the updated eventParticipants object.
-   */
-  subscribeToEventParticipants(eventId, callback) {
-    let ref = this.database().ref(`/eventParticipants/${eventId}`);
-    ref.on('value', snapshot => {
-      callback(snapshot.val());
-    });
-    this.subscriptions[`eventParticipants_${eventId}`] = ref;
-  }
-
-  /**
-   * Attend an event
-   * @param {string} eventId - The id of the event.
-   * @param {string} uid - The id of the user.
-   * @returns A Promise which resolves to a success message and rejects with an error.
-   */
-  attendEvent(eventId, uid) {
-    let self = this;
-    return new Promise((resolve, reject) => {
-      let updates = {};
-      updates[`/eventParticipants/${eventId}/${uid}`] = true;
-      updates[`/userAcceptedEvents/${uid}/${eventId}`] = true;
-      self.database().ref().update(updates)
-        .then(() => {
-          resolve('SUCCESS');
-        })
-        .catch(err => {
-          reject(err);
-        });
-    })
-  }
-
-  /**
-   * Unattend event
-   * @param {string} eventId - The id of the event.
-   * @param {string} uid - The id of the user.
-   * @returns A Promise which resolves to a success message and rejects with an error.
-   */
-  unattendEvent(eventId, uid) {
-    let self = this;
-    return new Promise((resolve, reject) => {
-      let updates = {};
-      updates[`/eventParticipants/${eventId}/${uid}`] = null;
-      updates[`/userAcceptedEvents/${uid}/${eventId}`] = null;
-      self.database().ref().update(updates)
-        .then(() => {
-          resolve('SUCCESS');
-        })
-        .catch(err => {
-          reject(err);
-        });
-    })
-  }
-
-  /**
-   * Subsribe to all events.
-   * @param {function} added - Function to be called when an event is added.
-   * @param {function} changed - Function to be called when an event is changed.
-   * @param {function} deleted - Function to be called when an event is deleted.
-   */
-  subscribeToEventList(added, changed, deleted) {
-    let ref = this.database().ref('/events');
-    ref.on('child_added', (snapshot => {
-      added(snapshot.val());
-    }));
-
-    ref.on('child_changed', (snapshot => {
-      changed(snapshot.val());
-    }));
-
-    ref.on('child_removed', (snapshot => {
-      deleted(snapshot.val());
-    }));
-
-    this.subscriptions['eventList'] = ref;
-  }
-
-  /**
-   * Clear all subscriptions.
-   */
-  clearSubscriptions() {
-    for (var key in this.subscriptions) {
-      if (this.subscriptions.hasOwnProperty(key) && this.subscriptions[key]) {
-        this.subscriptions[key].off();
-        this.subscriptions[key] = null;
-      }
+	/**
+	  * Clear all subscriptions.
+	*/
+    clearSubscriptions() {
+        for (var key in this.subscriptions) {
+            if (this.subscriptions.hasOwnProperty(key) && this.subscriptions[key]) {
+                this.subscriptions[key].off();
+                this.subscriptions[key] = null;
+            }
+        }
     }
   }
 
