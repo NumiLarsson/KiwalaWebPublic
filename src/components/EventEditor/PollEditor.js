b933b5954da1d3a929a9ff331c92e3ccc79a0d8a
Johan Lejdung <johanlejdung@gmail.com>
2017-03-13 15:49:18 +0100

    Resetting the form correctly

--- src/components/EventEditor/PollEditor.js
+++ src/components/EventEditor/PollEditor.js
@@ -4,9 +4,10 @@ import RaisedButton from 'material-ui/RaisedButton';
 import FontIcon from 'material-ui/FontIcon';
 import Dialog from 'material-ui/Dialog';
 import TextField from 'material-ui/TextField';
+import { Field, reduxForm, formValueSelector, reset } from 'redux-form';
 import CircularProgress from 'material-ui/CircularProgress';
 import {connect} from "react-redux";
-import { openPollEditor, closePollEditor } from '../../actions/eventeditor'
+import { initNewPollAndOpenEditor, closePollEditor } from '../../actions/eventeditor'
 import './styles/polleditor.css';
 
 const customContentStyle = {
@@ -19,11 +20,23 @@ class PollEditor extends Component {
     constructor() {
         super();
 
-        this.handleClose = this.handleClose.bind(this);
+        this.handleClose        = this.handleClose.bind(this);
+        this.handleSubmit        = this.handleSubmit.bind(this);
+        this.createNewPoll      = this.createNewPoll.bind(this);
+    }
+
+    createNewPoll() {
+        this.props.initNewPollAndOpenEditor();
     }
 
     handleClose() {
+        this.props.closePollEditor();
         //Use data from editor
+        this.props.resetForm('eventeditor-poll');
+    }
+
+    handleSubmit() {
+
     }
 
     render() {
@@ -31,26 +44,26 @@ class PollEditor extends Component {
           <FlatButton
             label="Cancel"
             primary={true}
-            onTouchTap={this.props.closePollEditor}
+            onTouchTap={this.handleClose}
           />,
           <FlatButton
             label="Submit"
             primary={true}
             disabled={true}
-            onTouchTap={this.handleClose}
+            onTouchTap={this.handleSubmit}
           />,
         ];
 
         return (
             <div>
-                <FlatButton onTouchTap={this.props.openPollEditor}
+                <FlatButton onTouchTap={this.createNewPoll}
                               label="Create Poll" backgroundColor="#fff"
                               icon={<FontIcon className="material-icons">poll</FontIcon>}/>
                 <Dialog
                     title="Create poll"
                     actions={actions}
                     open={this.props.open}
-                    onRequestClose={null}
+                    onRequestClose={this.handleClose}
                     autoScrollBodyContent={true}
                     contentClassName="polleditor-dialog"
                 >
@@ -62,7 +75,7 @@ class PollEditor extends Component {
                         <div style={{
                             marginTop: '1rem'
                         }}>
-                            {this.renderPage()}
+                            {this.renderPage(this.props.poll)}
                         </div>
                     )}
                 </Dialog>
@@ -70,27 +83,67 @@ class PollEditor extends Component {
         )
     }
 
-    renderPage() {
+    renderPage(poll) {
+
+        let pollChoices = [];
+
+        for (let choice in poll.choices) {
+            pollChoices.push(
+              <FlatButton key={choice} label={poll.choices[choice]} />
+            )
+        }
+
         return (
-            <div>
-            </div>
+            <form onSubmit={this.handleClose}>
+                <div className="polleditor-poll">
+                  <div className="polleditor-poll__question">
+                    <i className="material-icons color-gray">bubble_chart</i>
+                    <Field className="polleditor-poll__questionfield" placeholder="Question" name="poll_question" component="input" type="text" />
+                  </div>
+                  <div className="polleditor-poll__choices">
+                    {pollChoices}
+                  </div>
+                </div>
+            </form>
         );
     }
 }
 
+// Decorate the form component
+PollEditor = reduxForm({
+    form: 'eventeditor-poll', // a unique name for this form
+    enableReinitialize: true
+})(PollEditor);
+
 //Maps the state in our store to the props property of the Example object.
+const selector = formValueSelector('eventeditor-poll')
 const mapStateToProps = (state) => {
     return {
         open: state.eventeditor.polleditor.open,
-        loading: state.eventeditor.polleditor.loading
+        loading: state.eventeditor.polleditor.loading,
+        poll: {
+            active: selector(state, 'poll_active'),
+            question: selector(state, 'poll_question'),
+            choices: selector(state, 'poll_choices')
+        },
+        initialValues : {
+            poll_question: state.eventeditor.polleditor.poll.question,
+            poll_active: state.eventeditor.polleditor.poll.active,
+            poll_choices: state.eventeditor.polleditor.poll.choices
+        }
     }
 }
 
 //Wrapping the action creators in a dispatch call and allowing us to
 //access them through the props property of the Example object.
-const mapDispatchToProps = {
-    openPollEditor,
-    closePollEditor
+const mapDispatchToProps = (dispatch) => {
+  return {
+    resetForm: (formName) => {
+      dispatch(reset(formName))
+    },
+    initNewPollAndOpenEditor: () => dispatch(initNewPollAndOpenEditor()),
+    closePollEditor: () => dispatch(closePollEditor())
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(PollEditor);
